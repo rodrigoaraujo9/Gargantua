b363c54d61399b97cc9313576f39e3136c98913c
Rodrigo Araújo <rodrigogomesaraujo04@gmail.com>
2025-08-19 01:40:16 +0100

    now beem deletes itself when within event horizon

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use physical_constants::{NEWTONIAN_CONSTANT_OF_GRAVITATION, SPEED_OF_LIGHT_IN_VACUUM};
 use raylib::prelude::*;
-use std::f64::consts::PI;
 
 pub const SCREEN_WIDTH: i32 = 800;
 pub const SCREEN_HEIGHT: i32 = 600;
@@ -11,11 +10,6 @@ pub const SOLAR_MASS: f64 = 1.988416e30f64; //kg
 pub const SCALE_FACTOR: f64 = 3.4e-11f64; //scale down
 pub const SPEEDUP_FACTOR: f64 = 39235.0; // acceleration of frame so light movement is visible -> if 1, real time
 
-struct BlackHole {
-    position: Vector2,
-    mass: f64, //in solar masses
-}
-
 struct Beem {
     position: Vector2,
     angle: f32, // 0 - 2PI
@@ -23,10 +17,10 @@ struct Beem {
 
 impl Beem {
     const RADIUS: f32 = 5.0;
-    pub fn new(x: f32, y: f32, angle_: f32) -> Self {
+    pub fn new(x: f32, y: f32, angle: f32) -> Self {
         Self {
             position: Vector2::new(x, y),
-            angle: angle_,
+            angle,
         }
     }
 
@@ -49,21 +43,49 @@ impl Beem {
         }
     }
 
+    //collision detection
+    fn within_event_horizon(&self, black_hole: &BlackHole) -> bool {
+        let distance = ((self.position.x - black_hole.position.x).powi(2)
+            + (self.position.y - black_hole.position.y).powi(2))
+        .sqrt();
+        distance <= black_hole.event_horizon_radius() as f32
+    }
+
     pub fn draw(&self, d: &mut RaylibDrawHandle) {
         d.draw_circle_v(self.position, Self::RADIUS, Color::WHITE);
     }
 }
 
+impl Drop for Beem {
+    fn drop(&mut self) {
+        // destructor
+    }
+}
+
+struct BlackHole {
+    position: Vector2,
+    mass: f64,   //in solar masses
+    radius: f64, // in pixels
+}
+
 impl BlackHole {
-    pub fn new(x: f32, y: f32, mass_: f64) -> Self {
+    pub fn new(x: f32, y: f32, mass: f64) -> Self {
         Self {
             position: Vector2::new(x, y),
-            mass: mass_,
+            mass,
+            radius: Self::event_horizon_radius_init(mass),
         }
     }
-    // r_s
+    // public r_s
     pub fn event_horizon_radius(&self) -> f64 {
         let mass_kg: f64 = self.mass * SOLAR_MASS;
+        let r_s: f64 = SCALE_FACTOR * (2.0 * G * mass_kg) / (C * C);
+        r_s
+    }
+
+    // r_s used for init
+    fn event_horizon_radius_init(mass: f64) -> f64 {
+        let mass_kg: f64 = mass * SOLAR_MASS;
         let r_s: f64 = SCALE_FACTOR * (2.0 * G * mass_kg) / (C * C);
         r_s
     }
@@ -83,14 +105,20 @@ fn main() {
     let sh: f32 = SCREEN_HEIGHT as f32;
     let sw: f32 = SCREEN_WIDTH as f32;
     let gargantula = BlackHole::new(sw / 2.0, sh / 2.0, 10e8);
-    let mut beem = Beem::new(0.0, sh / 2.0, 0.0);
+    let mut beem: Option<Beem> = Some(Beem::new(0.0, sh / 2.0, 0.0));
 
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
-        d.clear_background(Color::DARKGRAY);
+        d.clear_background(Color::LIGHTGRAY);
+        if let Some(ref mut beam) = beem {
+            beam.update(&mut d);
+            beam.draw(&mut d);
+            if beam.within_event_horizon(&gargantula) {
+                beem = None;
+            }
+        }
         gargantula.draw(&mut d);
-        beem.draw(&mut d);
-        beem.update(&mut d);
+
         let mass_text = format!("mass: {:.1e} solar masses", gargantula.mass);
         let radius_km = gargantula.event_horizon_radius() / SCALE_FACTOR;
         let radius_text = format!("event horizon: {:.1} km", radius_km);
